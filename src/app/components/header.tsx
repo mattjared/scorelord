e2d5a0d5326b6b8d212b3e064ded44b09b261551
Matt Jared <matt.jared@vercel.com>
2025-01-26 15:29:21 -0600

    Blue Edwards

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="flex flex-col py-10">
       <h1 className="text-5xl font-bold text-teal-500 tracking-tight mb-2">Welcome to Scorelord</h1>
       <p className="text-white mb-8">All the scores. None of the ads. The cauldron is calling.</p>
-      <Image src="/mascot.png" alt="Scorelord Logo" width={250} height={250} />
+      <Image src="/logo.webp" alt="Scorelord Logo" width={400} height={400} />
     </div>
   )
 }
